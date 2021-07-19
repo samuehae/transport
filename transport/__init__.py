9c3e8fd5f8ef28fef9a62a080ce80e23f4b872f8
Samuel Haeusler <samuehae@users.noreply.github.com>
2021-07-19 17:10:47 +0200

    populate namespace for package import via __init__.py

---
+++ transport/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+
+# import relevant modules
+from . import solvers
+from . import scatter1d
+
+
+# make selected functions available in package namespace
+from .scatter1d import amplitudes
+from .scatter1d import wavefunction
